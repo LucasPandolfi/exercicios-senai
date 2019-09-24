d57e02e69ba58ee7b44cb21ecc90a1aa9d7fc418
lucas pandolfi <lucasdatealive@gmail.com>
2019-09-23 22:11:29 -0300

    novas alterações0.1

--- Exercícios/menu_calculador/Program.cs
+++ Exercícios/menu_calculador/Program.cs
@@ -15,7 +15,8 @@ namespace menu_calculador
             string retangulo = "5";
             string losango = "6";
             string formas = "";
-
+            char op;
+        do{ 
             Console.WriteLine("Figuras geométricas e suas medidas. Selecione a figura desejada: ");
             Console.Write("1. Quadrado, 2. Triângulo, 3. círculo, 4. trapézio, 5. retângulo, 6. losango: ");
             formas = Console.ReadLine();
@@ -25,8 +26,8 @@ namespace menu_calculador
 
                 Console.Write("medida do lado :");
                 lado_quadrado = double.Parse(Console.ReadLine());
-                double resultado = lado_quadrado * lado_quadrado;
-                Console.Write("Área do quadrado = " + resultado);
+                double resultado_quadrado = lado_quadrado * lado_quadrado;
+                Console.Write("Área do quadrado = " + resultado_quadrado);
             } 
             else if(formas == "2"){
                 double base_triangulo = 0;
@@ -36,13 +37,13 @@ namespace menu_calculador
                 base_triangulo = double.Parse(Console.ReadLine());
                 Console.Write("Altura do trinagulo: ");
                 altura_triangulo = double.Parse(Console.ReadLine());
-                double resultado1 = base_triangulo * base_triangulo;
-                Console.Write("Área do triângulo = " + resultado1);
+                double resultado_triangulo = base_triangulo * altura_triangulo;
+                Console.Write("Área do triângulo = " + resultado_triangulo);
             }
             else if(formas == "3"){ 
-            double raio = 0;
+                double raio = 0;
 
-             Console.Write("medida do raio : ");
+                Console.Write("medida do raio : ");
                 raio = double.Parse(Console.ReadLine());
                 double raio_2 = raio * raio;
                 double resultado_raio = 3.14 * raio_2;
@@ -54,11 +55,10 @@ namespace menu_calculador
                 double base_menor = 0;
                 double altura_trapezio = 0;
                 double media;
-                double resultado;
+                double resultado_trapezio;
 
                 Console.Write("medida da base maior : ");
                 base_maior = double.Parse(Console.ReadLine());
-                
                 Console.Write("medida da base menor : ");
                 base_menor = double.Parse(Console.ReadLine());
 
@@ -66,9 +66,38 @@ namespace menu_calculador
                 
                 Console.Write("medida da altura : ");
                 altura_trapezio = double.Parse(Console.ReadLine());
-                resultado = media * altura_trapezio;
-                 Console.Write("Área do trapézio = " + resultado);
+                resultado_trapezio = media * altura_trapezio;
+                Console.Write("Área do trapézio = " + resultado_trapezio);
                 }
+
+            else if(formas == "5"){
+                double base_retangulo = 0;
+                double altura_retangulo = 0;
+                double resultado2 = 0;
+
+                Console.Write("medida da base: ");
+                base_retangulo = double.Parse(Console.ReadLine());
+                Console.Write("medida da altura: ");
+                altura_retangulo = double.Parse(Console.ReadLine());
+                resultado2 = base_retangulo * altura_retangulo;
+                Console.Write("Área do retangulo = " + resultado2);
+                }
+
+                else if(formas == "6"){
+                    double diagonal_maior;
+                    double diagonal_menor;
+                    double resultado_diagonal;
+
+                    Console.Write("medida da diagonal maior: ");
+                    diagonal_maior = double.Parse(Console.ReadLine());
+                    Console.Write("medida da diagonal menor: ");
+                    diagonal_menor = double.Parse(Console.ReadLine());
+                    resultado_diagonal = (diagonal_maior * diagonal_menor) / 2;
+                    Console.Write("Área do losango = " + resultado_diagonal);
+                }
+                Console.Write("\n Deseja voltar do ínicio e escolher outra figura geométrica? Se sim, digite qualquer caractere. Se não digite N ou n: \n");
+                op = char.Parse(Console.ReadLine());
+        }while(op !='N' && op !='n');
         }
     }
 }
