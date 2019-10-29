17c7bba5c28bbe8a1609230688bceb72e8736273
lucas pandolfi <lucasdatealive@gmail.com>
2019-10-28 21:41:44 -0300

    Alterações que vão dar pau

--- DesafiosDeProgramacao/CaixaEletronico/Program.cs
+++ DesafiosDeProgramacao/CaixaEletronico/Program.cs
@@ -6,19 +6,38 @@ namespace CaixaEletronico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insira um valor de retirada: ");
-            int Retirada = int.Parse(Console.ReadLine());
+            Console.WriteLine("Caixa Eletronico");
+            System.Console.WriteLine();
+            var valor = 0;
 
-            int[] Notas = new int[5];
-            Notas[0] = 5;
-            Notas[1] = 10;
-            Notas[2] = 20;
-            Notas[3] = 50;
-            Notas[4] = 100;
 
-            for(int i = 0; i < Retirada; i++)
+            Console.Write("Digite o valor do saque: ");
+            if (int.TryParse(Console.ReadLine(), out valor))
             {
-                //TODO: Terminar o exercício.
+                int notas100 = valor / 100;
+                int resto = valor - (notas100 * 100);
+
+                int notas50 = resto / 50;
+                resto -= notas50 * 50;
+
+                int notas20 = resto / 20;
+                resto -= notas20 * 20;
+
+                int notas10 = resto / 10;
+                resto -= notas10 * 10;
+
+                int notas5 = resto / 5;
+                resto -= notas5 * 5;
+                
+                int notas1 = resto / 1;
+                resto -= notas1 * 1;
+                
+                Console.WriteLine($"\nQuantidade de notas:\nNotas de 100R$:  {notas100}\n");
+                Console.WriteLine($"Notas de 50R$:  {notas50}\n");
+                Console.WriteLine($"Notas de 20R$:  {notas20}\n");
+                Console.WriteLine($"Notas de 10R$:  {notas10}\n");
+                Console.WriteLine($"Notas de 5R$:  {notas5}\n");
+                Console.WriteLine($"Notas de 1R$:  {notas1}\n");
             }
         }
     }
