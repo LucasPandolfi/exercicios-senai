2d63060dc02142ce282b0c483a020074df96237f
Lucas Pandolfi <lucasdatealive@gmail.com>
2019-10-30 17:19:33 -0300

    Alterções

---
+++ DesafiosDeProgramacao/Exercicio24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercicio24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vetores");
+            System.Console.WriteLine();
+            int[] Numeros = new int[10];
+            int[] Numeros2 = new int[10];
+        }
+    }
+}
