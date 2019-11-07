cbe1112ad3218f42b97b56ceb0587143c6d7c640
Lucas Pandolfi <lucasdatealive@gmail.com>
2019-11-07 18:17:25 -0200

    Alterações

---
+++ DesafiosDeProgramacao/Exercicio27/Models/Listar.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Exercicio27.Models
+{
+    public class Listar
+    {
+        public string Nome;
+        public int DataNasc;
+
+        public Listar(string Nome, int DataNasc)
+        {
+            this.Nome = Nome;
+            this.DataNasc = DataNasc;
+        }
+    }
+}
