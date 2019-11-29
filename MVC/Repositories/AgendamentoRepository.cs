6f75ea15259244c2995df360a11e3b4e4cffad55
Lucas Pandolfi <lucasdatealive@gmail.com>
2019-11-29 15:46:35 -0300

    alteraçoes

---
+++ MVC/Repositories/AgendamentoRepository.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using MVC.Models;
+
+namespace MVC.Repositories
+{
+    public class AgendamentoRepository : RepositoryBase
+    {
+        private const string PATH = "Database/Agendamento.csv";
+
+        public AgendamentoRepository()
+        {
+            if(!File.Exists(PATH))
+            {
+                File.Create(PATH).Close();
+            }
+        }
+
+        public bool Inserir(Eventos eventos)
+        {
+            return true;
+        }
+    }
+}
